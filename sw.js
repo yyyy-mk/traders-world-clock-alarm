4c9d9e27bc44a26684ca9907dcf38214e89bc7e0
yyyy-mk <gigforthesky@gmail.com>
2026-02-10 13:22:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
     event.waitUntil(
-        clients.matchAll({type: 'window'}).then(windowClients => {
+        clients.matchAll({type: 'window'}).then( windowClients => {
             if (windowClients.length > 0) {
                 windowClients[0].focus();
             } else {
